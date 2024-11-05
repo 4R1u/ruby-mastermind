81f4b3c79e514eb3ad4f50af9ba2f732d1316ab5
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-11-05 18:34:01 +0400

    Declare colors Class Var in Code

--- lib/code.rb
+++ lib/code.rb
@@ -4,6 +4,7 @@ require 'colorize'
 
 # Sequence of colors.
 class Code
+  @@colors = [0, :red, :yellow, :green, :blue, :orange, :purple]
   def initialize(arr)
     return unless arr.all?((1..6)) && arr.length == 4
 
