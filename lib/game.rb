0f57e372e9c6da861c3a336d34e757704ba835a4
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-11-06 11:23:52 +0400

    Add Basic Game Class

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,10 @@
 # frozen_string_literal: true
+
+require_relative 'code'
+
+# This represents the board on which the game is played.
+class Game
+  def initialize(code = Code.new([]))
+    @code = code
+  end
+end
