3b149d53e10d76762d80c97bc136a5be2d448e2d
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-11-09 13:54:56 +0400

    Create Basic Player#attempt Function

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'game'
+
 # TODO: implement player being able to create the code
 # Represents a player
 class Player
   def initialize
     @wins = 0
+  end
+
+  def attempt(game, arr)
+    try = game.guess(arr)
+    @wins += 1 if try[:red] == 4
   end
 end
