3315400fe7aaa557d4705e6439ddb8321877b92d
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-11-09 13:59:31 +0400

    Add win_state Var to Game

--- lib/game.rb
+++ lib/game.rb
@@ -4,21 +4,28 @@ require_relative 'code'
 
 # This represents the board on which the game is played.
 class Game
-  attr_reader :state
+  attr_reader :state, :win_state
 
   def initialize(code = Code.new)
     @code = code.is_a?(Code) ? code : Code.new
     @guesses_left = 10
     @state = ''
+    @win_state = 'Indecisive'
   end
 
   def guess(arr)
-    return unless @guesses_left.positive?
+    return if @win_state == 'Won'
+
+    unless @guesses_left.positive?
+      @win_state = 'Lost'
+      return
+    end
 
     @guesses_left -= 1
     result = @code.compare(arr)
     puts @state += Code.new(arr).view << ' ' <<
                    view_result(result) << "\n"
+    @win_state = 'Won' if result[:red] == 4
     result
   end
 
