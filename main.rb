541c2855f7f6f8eeb8817604bf26f5511e51544b
Saad Nawaz Ghauri <snghauri5@gmail.com>
2024-11-10 13:06:03 +0400

    Implement Basic Game Loop

--- main.rb
+++ main.rb
@@ -5,3 +5,16 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 human = Player.new
+
+loop do
+  game = Game.new
+  loop do
+    attempt = ''
+    puts 'Enter a string of 4 numbers, all between 1 and 6 inclusive'
+    attempt = gets.chomp until Code.valid_guess?(attempt)
+    human.attempt(game, attempt.chars.map(&:to_i))
+    break unless game.win_state == 'Indecisive'
+  end
+  puts 'Would you like to play again? [y]'
+  break unless gets.chomp.downcase == 'y'
+end
