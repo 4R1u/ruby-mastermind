d56cdcf6850751f2591112b8dda6161d04b2803d
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-11 06:44:49 +0400

    Call super in Bot#initialize

--- lib/bot.rb
+++ lib/bot.rb
@@ -5,7 +5,7 @@ require_relative 'game'
 # Represents the Computer Player Trying to Guess A Game
 class Bot < Player
   def initialize
-    super.initialize
+    super
     @all_possible_solutions = []
     1296.times do |i|
       @all_possible_solutions <<
