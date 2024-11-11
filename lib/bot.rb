1394a0766da7164e94b4b3cff2a53686c9475d34
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-11 09:51:30 +0400

    Move Making the Array of Possible Solutions into a Private Function

--- lib/bot.rb
+++ lib/bot.rb
@@ -4,12 +4,14 @@ require_relative 'game'
 
 # Represents the Computer Player Trying to Guess A Game
 class Bot < Player
-  def initialize
-    super
-    @all_possible_solutions = []
+  private
+
+  def array_of_possible_solutions
+    all_possible_solutions = []
     1296.times do |i|
-      @all_possible_solutions <<
+      all_possible_solutions <<
         [i / 6 / 6 / 6 % 6, i / 6 / 6 % 6, i / 6 % 6, i % 6]
     end
+    all_possible_solutions
   end
 end
