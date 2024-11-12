b89440c9e4aae37106f692b49b8aace0f3fa9448
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-12 18:23:22 +0400

    Fix Typos in Bot

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'game'
+require_relative 'player'
 
 ALL_GUESSES = (0..1295).map do |i|
   [(i / 6 / 6 / 6 % 6) + 1,
@@ -22,13 +23,13 @@ class Bot < Player
   def next_guess(prev_guess, solution_array)
     maxima_in_all = ALL_GUESSES.map do |guess|
       eliminate_responses(
-        solution_array, guess, Code.new(prev_guess).compare(guess)
+        solution_array, Code.new(guess), Code.new(prev_guess).compare(guess)
       ).length
     end
     minimum = maxima_in_all.min
     solution_array.each do |guess|
       return guess if maxima_in_all[ALL_GUESSES.index(guess)] == minimum
     end
-    ALL_GUESSES[minimum_in_all.index(minimum)]
+    ALL_GUESSES[maxima_in_all.index(minimum)]
   end
 end
