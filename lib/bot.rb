fff6d50dc0d057f0544fcaa0f5bd71f3bc2ea79e
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-12 15:53:42 +0400

    Implement Bot#next_guess Function

--- lib/bot.rb
+++ lib/bot.rb
@@ -18,4 +18,17 @@ class Bot < Player
       comparison_code.compare(code) == response_desired
     end
   end
+
+  def next_guess(prev_guess, solution_array)
+    maxima_in_all = ALL_GUESSES.map do |guess|
+      eliminate_responses(
+        solution_array, guess, Code.new(prev_guess).compare(guess)
+      ).length
+    end
+    minimum = maxima_in_all.min
+    solution_array.each do |guess|
+      return guess if maxima_in_all[ALL_GUESSES.index(guess)] == minimum
+    end
+    minimum
+  end
 end
