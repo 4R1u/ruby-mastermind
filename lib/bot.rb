4475a9eea12b2212733112febb7dfd98f0ccb8a8
4R1u <62248461+4R1u@users.noreply.github.com>
2024-11-15 12:03:42 +0400

    Make Bot#user_feedback Actually Print Guess

--- lib/bot.rb
+++ lib/bot.rb
@@ -27,7 +27,7 @@ class Bot < Player
   private
 
   def user_feedback(guess)
-    Code.new(guess).view
+    puts Code.new(guess).view
     red, white = 8
     puts 'Enter the number of red pegs'
     red = gets.to_i until (0..4).include?(red)
